4b27716e673ceae003a47a6540b871165f169d10
catoalkodi <catoal.kodi@gmail.com>
2016-08-19 18:04:21 +0200

    new

--- plugin.video.SportsDevil/lib/utils/unpack95High.py
+++ plugin.video.SportsDevil/lib/utils/unpack95High.py
@@ -50,13 +50,11 @@ def unpack(source):
 
 def _filterargs(source):
     """Juice from a source file the four args needed by decoder."""
-    argsregex = (r".*'(.*)'\.split")
+    argsregex = (r"}\('(.*)', *(\d+), *(\d+), *'(.*)'\.")
     args = re.search(argsregex, source, re.DOTALL).groups()
-    ar2 = (r".*?'([\xa1-\xff].*}\);)")
-    args2 = re.search(ar2, source, re.DOTALL).groups()
-    
+
     try:
-        return args2[0], args[0].split('|'), len(args[0].split('|')), len(args[0].split('|'))
+        return args[0], args[3].split('|'), int(args[1]), int(args[2])
     except ValueError:
         raise UnpackingError('Corrupted p.a.c.k.e.r. data.')
 
